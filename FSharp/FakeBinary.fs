e24e83a9bd3501355249542af27d18064aba1e5f
Adam Pridmore <adam_pridmore@hotmail.com>
2018-03-11 17:27:38 +0000

    - Implemented fake binary

---
+++ FSharp/FakeBinary.fs
@@ -0,0 +1,18 @@
+module FakeBinary
+
+open Xunit
+open System
+
+// https://www.codewars.com/kata/fake-binary/train/fsharp
+let fakeBin (x:string) = 
+    let convert i = if i < 5 then "0" else "1"
+
+    x.ToCharArray()
+    |> Seq.map (string >> Int32.Parse >> convert)
+    |> Seq.reduce (sprintf "%s%s")
+
+[<Fact>]
+let suite =
+    Assert.Equal("01011110001100111", fakeBin "45385593107843568" )
+    Assert.Equal("101000111101101", fakeBin "509321967506747" )
+    Assert.Equal("011011110000101010000011011", fakeBin "366058562030849490134388085" )

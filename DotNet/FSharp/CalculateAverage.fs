8d5a373f746c715d44eed0cff4f5b54d454b9f82
Adam Pridmore <adam_pridmore@hotmail.com>
2018-04-03 21:31:56 +0100

    - Added more solutions.

---
+++ DotNet/FSharp/CalculateAverage.fs
@@ -0,0 +1,8 @@
+﻿module CalculateAverage
+
+let avg list = 
+  list 
+  |> Seq.fold (fun (sum, count) x -> ( (sum+x) , (count + 1 ) ) ) (0.0 , 0)
+  |> (fun (sum, count) -> sum / (count |> float) )
+
+[1.0;1.0;1.0] |> avg
---
+++ DotNet/FSharp/DescendingOrder.fs
@@ -0,0 +1,15 @@
+﻿module DescendingOrder
+
+let descendingOrder n =  
+    let toCharArray (s:string) = s.ToCharArray()
+        
+    n
+    |> string
+    |> toCharArray
+    |> Seq.map string
+    |> Seq.map (System.Int32.Parse)
+    |> Seq.sortBy id
+    |> Seq.map (string)
+    |> String.concat ""
+    |> (fun s -> System.Int32.Parse(s) )
+    
